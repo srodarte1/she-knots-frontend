4f5ae7c393d8a1bbea9d64dc73f2eb1b0b1014bf
+ <rodarte_sarah@yahoo.com>
2023-01-25 18:52:12 -0700

    test

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,30 @@
 const Cart = () => {
     return(
-        <div>
+        <div className="cart-table">
+            <table>
+            <h2 className="cart-title">Your Cart [0]</h2>
+        <tr>
+          <th>Item</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total</th>
+        </tr>
+        <tr>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Megha</td>
+          <td>19</td>
+          <td>Female</td>
+        </tr>
+        <tr>
+          <td>Subham</td>
+          <td>25</td>
+          <td>Male</td>
+        </tr>
+      </table>
             
         </div>
     )
